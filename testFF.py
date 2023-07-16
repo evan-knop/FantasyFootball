7342deb39b3f13af32b6d223fb243f8d08949d36
evan-knop <eknop23@gmail.com>
2023-07-16 09:10:44 -0500

    Scraping code in loop

--- testFF.py
+++ testFF.py
@@ -3,24 +3,39 @@ import re
 import pandas as pd
 from bs4 import BeautifulSoup
 
-stat_cat_URLs_22 = ["https://www.pro-football-reference.com/years/2022/passing.htm", 
-                    "https://www.pro-football-reference.com/years/2022/rushing.htm",
-                    "https://www.pro-football-reference.com/years/2022/receiving.htm"
-                    ]
+base_url = "https://www.pro-football-reference.com/years/"
+years_to_pull = ['2020', '2021', '2022']
+stats_to_pull = ['passing', 'rushing', 'receiving']
 
-URL = "https://www.pro-football-reference.com/years/2022/receiving.htm"
-page = requests.get(URL)
+for year in years_to_pull:
+    for stat in stats_to_pull:
+        current_URL = base_url + year + "/" + stat + ".htm"
+        page = requests.get(current_URL)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        results = soup.find("tbody")
+        data_rows = results.find_all("tr")
+
+        row_data = [[td.getText() for td in data_rows[i].findAll('td')]
+                    for i in range(len(data_rows))]
+        
+        df = pd.DataFrame(row_data)
+        df.to_csv(stat + "_" + year + ".csv", index = False)
+
+
+#URL = "https://www.pro-football-reference.com/years/2022/receiving.htm"
+""" page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find("tbody")
-0
 data_rows = results.find_all("tr")
 
 row_data = [[td.getText() for td in data_rows[i].findAll('td')]
                     for i in range(len(data_rows))]
-
-# create the dataframe
+ """
+""" # create the dataframe
 passing = pd.DataFrame(row_data)
 # export dataframe to a CSV 
-passing.to_csv("receiving.csv", index=False)
+passing.to_csv("receiving.csv", index=False) """
