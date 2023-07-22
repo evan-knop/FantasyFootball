7358431ce4a45189893a195f92d6d92197876b62
evan-knop <eknop23@gmail.com>
2023-07-22 16:25:32 -0500

    player data etl to MySQL setup

---
+++ player_etl.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import pandas as pd
+
+#Connecting to MySQL DB - local host
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "FANTASY_FOOTBALL"
+)
+#Load CSV
+df = pd.read_csv("PlayerInfo.csv")
+
+cursor = conn.cursor()
+
+#Truncate table before reloading
+cursor.execute("TRUNCATE TABLE FANTASY_FOOTBALL.players")
+
+ #loop through the data frame
+for i,row in df.iterrows():
+    sql = "INSERT INTO FANTASY_FOOTBALL.players VALUES (%s,%s)"
+    cursor.execute(sql, tuple(row))
+    print("Record inserted")
+    conn.commit()
+
+conn.close()
+
