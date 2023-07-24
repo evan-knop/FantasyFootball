ad34bc8797a0ec811c7d763b45cb8dba51d737ad
evan-knop <eknop23@gmail.com>
2023-07-24 18:38:40 -0500

    etl jobs working for receiving and rushing

---
+++ rushing_etl.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import pandas as pd
+import glob
+
+#Connecting to MySQL DB - local host
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "FANTASY_FOOTBALL"
+)
+
+#Create cursor object
+cursor = conn.cursor()
+
+#Load CSVs
+all_files = glob.glob("rushing*.csv")
+
+#Concatenate all files into one
+df = pd.concat([pd.read_csv(f, on_bad_lines='skip') for f in all_files]) 
+
+#Change nan values to None
+df = df.where((pd.notnull(df)), None)
+df = df.fillna(0)
+
+#Truncate table before reloading
+cursor.execute("TRUNCATE TABLE FANTASY_FOOTBALL.rushing")
+
+ #loop through the data frame
+for i,row in df.iterrows():
+    #Skip the empty rows from scraping header rows off of FBRef
+    if len(row[0]) <= 4:
+        continue
+    sql = "INSERT INTO FANTASY_FOOTBALL.rushing VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(sql, tuple(row))
+    print("Record inserted")
+    conn.commit()
+
+conn.close()
+
