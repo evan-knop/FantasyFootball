6e0e4f9df8b29ef7ca9e8a8cd0c270c2cc778a78
evan-knop <eknop23@gmail.com>
2023-07-27 16:10:11 -0500

    Updated player scraping to pull in more data

--- FF_Scraper.py
+++ FF_Scraper.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 base_url = "https://www.pro-football-reference.com/years/"
 years_to_pull = ['2020', '2021', '2022']
 stats_to_pull = ['passing', 'rushing', 'receiving']
-player_info = []
 
 for year in years_to_pull:
     for stat in stats_to_pull:
@@ -19,30 +18,41 @@ for year in years_to_pull:
         data_rows = results.find_all("tr")
 
         'Parse Player info for players table'
-        for row in data_rows:
+        """ for row in data_rows:
             if row.find('td'):
                 player_info = player_info + ([((row.find('td')['data-append-csv'], row.find('td').getText()
-                                                                    .replace('*', '').replace('+','')))])
+                                                                    .replace('*', '').replace('+','')))]) """
         'Parse stats info for stats tables'
         i=0    
         player_stats = [[]]
+        player_info = [[]]
 
         for j in range(len(data_rows)):
             pstats = data_rows[j].findAll('td')
             for i in range(len(pstats)):
                 if i == 0:
                     player_stats[j].append(pstats[0]['data-append-csv'])
+                    player_info[j].append(pstats[0]['data-append-csv'])
+                    player_info[j].append(pstats[0].getText())
                     i+=1 
+                elif i >=1 and i<=5:
+                    player_stats[j].append(pstats[i].getText().replace('*', '').replace('+',''))
+                    player_info[j].append(pstats[i].getText().replace('*', '').replace('+',''))
+                    i+=1
                 else:
                     player_stats[j].append(pstats[i].getText().replace('*', '').replace('+',''))
                     i+=1
             j+=1
             player_stats.append([])
+            player_info.append([])
         
 
         'Add year column to end of stats data sets'
         for row in player_stats:
             row.append(year)   
+        'Add year column to end of player data sets'
+        for row in player_info:
+            row.append(year)
 
         'Create CSVs for stats'
         df = pd.DataFrame(player_stats)
