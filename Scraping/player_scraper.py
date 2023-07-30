509b5d6ffc0c816e5c15d708fc2c92327314f0b6
evan-knop <eknop23@gmail.com>
2023-07-29 20:59:09 -0500

    divs scraped out for indiv player profiles

--- Scraping/player_scraper.py
+++ Scraping/player_scraper.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from string import ascii_uppercase
 
-base_url = "https://www.pro-football-reference.com/players/"
+""" base_url = "https://www.pro-football-reference.com/players/"
 player_url_list = []
 
 #Loop through every letter in the alphabet
@@ -11,7 +11,7 @@ for letter in ascii_uppercase:
     current_url = base_url + letter
 
     #Prevent FB ref from blocking us
-    time.sleep(5)
+    time.sleep(10)
 
     page = requests.get(current_url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -21,3 +21,26 @@ for letter in ascii_uppercase:
     for player in players:
         if "2022" in player.getText():
             player_url_list.append(player.find("a")['href'])
+
+'Create CSVs for stats'
+df = pd.DataFrame(player_url_list)
+df.to_csv("CSVs/PlayerURLList.csv", index = False)
+ """
+
+base_url = "https://www.pro-football-reference.com/"
+player_data = pd.read_csv('CSVs/PlayerURLList.csv', header = 0)  
+
+player_list = player_data.values.tolist()
+
+for player in player_list:
+    current_url = base_url + player[0]
+
+    page = requests.get(current_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    player_divs = soup.find_all("div")
+
+    i=1 
+    #Prevent FB ref from blocking us
+    time.sleep(7)
+
