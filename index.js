5a9d1f376f5f79eba2deaac22733353423993a55
evan-knop <eknop23@gmail.com>
2023-08-20 22:00:17 -0500

    v

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './ff/build')));
 
 
 // MySQL configuration  
